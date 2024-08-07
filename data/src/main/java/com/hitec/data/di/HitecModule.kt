1c032ab9a42398251d36304f89bf6b749b22010a
E1jeong <won9964@gmail.com>
2024-08-07 16:12:56 +0900

    add: Di module GetInstallDeviceUseCase & DeleteInstallDbUseCase

--- data/src/main/java/com/hitec/data/di/HitecModule.kt
+++ data/src/main/java/com/hitec/data/di/HitecModule.kt
@@ -1,13 +1,17 @@
 package com.hitec.data.di
 
+import com.hitec.data.usecase.DeleteInstallDbUseCaseImpl
 import com.hitec.data.usecase.FindLocalSiteNameUseCaseImpl
 import com.hitec.data.usecase.GetInstallDbUrlUseCaseImpl
 import com.hitec.data.usecase.GetInstallDbUseCaseImpl
+import com.hitec.data.usecase.GetInstallDeviceUseCaseImpl
 import com.hitec.data.usecase.GetLocalSiteUseCaseImpl
 import com.hitec.data.usecase.GetSubAreaUseCaseImpl
+import com.hitec.domain.usecase.DeleteInstallDbUseCase
 import com.hitec.domain.usecase.FindLocalSiteNameUseCase
 import com.hitec.domain.usecase.GetInstallDbUrlUseCase
 import com.hitec.domain.usecase.GetInstallDbUseCase
+import com.hitec.domain.usecase.GetInstallDeviceUseCase
 import com.hitec.domain.usecase.GetLocalSiteUseCase
 import com.hitec.domain.usecase.GetSubAreaUseCase
 import dagger.Binds
@@ -18,19 +22,18 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class HitecModule {
-
     @Binds
     abstract fun bindGetLocalSiteUseCase(uc: GetLocalSiteUseCaseImpl): GetLocalSiteUseCase
-
     @Binds
     abstract fun bindFindLocalSiteNameUseCase(uc: FindLocalSiteNameUseCaseImpl): FindLocalSiteNameUseCase
-
     @Binds
     abstract fun bindGetSubAreaUseCase(uc: GetSubAreaUseCaseImpl): GetSubAreaUseCase
-
     @Binds
     abstract fun bindGetInstallDbUrlUseCase(uc: GetInstallDbUrlUseCaseImpl): GetInstallDbUrlUseCase
-
     @Binds
     abstract fun bindGetInstallDbUseCase(uc: GetInstallDbUseCaseImpl): GetInstallDbUseCase
+    @Binds
+    abstract fun bindGetInstallDeviceUseCase(uc: GetInstallDeviceUseCaseImpl): GetInstallDeviceUseCase
+    @Binds
+    abstract fun bindDeleteInstallDbUseCase(uc: DeleteInstallDbUseCaseImpl): DeleteInstallDbUseCase
 }
