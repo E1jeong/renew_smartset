3020cab77611db286ec9f90a19cfbc91c0575aab
E1jeong <won9964@gmail.com>
2024-08-07 16:06:44 +0900

    chore: DownloadInstallDbResponse - reformat

--- data/src/main/java/com/hitec/data/model/DownloadInstallDbResponse.kt
+++ data/src/main/java/com/hitec/data/model/DownloadInstallDbResponse.kt
@@ -6,28 +6,18 @@ import com.tickaroo.tikxml.annotation.Xml
 
 @Xml(name = "DownloadInstallDBReponse") //오타 주의
 data class DownloadInstallDbResponse(
-    @PropertyElement(name = "userId")
-    val userId: String,
-    @PropertyElement(name = "userLevel")
-    val userLevel: String,
-    @PropertyElement(name = "localSite")
-    val localSite: String,
-    @PropertyElement(name = "returnCode")
-    val returnCode: Int,
-    @PropertyElement(name = "msgCode")
-    val msgCode: Int,
-    @Element(name = "InstallDBInfo")
-    val installDBInfo: List<InstallDbInfo>,
+    @PropertyElement(name = "userId") val userId: String,
+    @PropertyElement(name = "userLevel") val userLevel: String,
+    @PropertyElement(name = "localSite") val localSite: String,
+    @PropertyElement(name = "returnCode") val returnCode: Int,
+    @PropertyElement(name = "msgCode") val msgCode: Int,
+    @Element(name = "InstallDBInfo") val installDBInfo: List<InstallDbInfo>,
 )
 
 @Xml(name = "InstallDBInfo")
 data class InstallDbInfo(
-    @PropertyElement(name = "rowNumber")
-    val rowNumber: Int,
-    @PropertyElement(name = "localSite")
-    val localSite: String,
-    @PropertyElement(name = "areaCd")
-    val areaCd: String?,
-    @PropertyElement(name = "siteUrl")
-    val siteUrl: String,
+    @PropertyElement(name = "rowNumber") val rowNumber: Int,
+    @PropertyElement(name = "localSite") val localSite: String,
+    @PropertyElement(name = "areaCd") val areaCd: String?,
+    @PropertyElement(name = "siteUrl") val siteUrl: String,
 )
