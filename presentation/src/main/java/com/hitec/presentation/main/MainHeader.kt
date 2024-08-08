737852927b6548e9919275d9087abfeba356f7b0
E1jeong <won9964@gmail.com>
2024-08-08 12:50:55 +0900

    modify: develop and apply MainHeader in MainNavHost

---
+++ presentation/src/main/java/com/hitec/presentation/main/MainHeader.kt
@@ -0,0 +1,53 @@
+package com.hitec.presentation.main
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.hitec.presentation.util.NavigationUtils
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainHeader(
+    viewModel: MainViewModel,
+    navController: NavHostController,
+    currentRoute: String?
+) {
+    TopAppBar(
+        title = {
+            Text(text = NavigationUtils.findDestination(currentRoute).title)
+        },
+        navigationIcon = {
+            if (!MainRoute.isMainRoute(currentRoute)) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.Filled.ArrowBack, "BackIcon")
+                }
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        ),
+        actions = {
+            if (currentRoute == MainRoute.InstallDevice.route || currentRoute == MainRoute.AsDevice.route) {
+                IconButton(
+                    onClick = { }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "SearchIcon"
+                    )
+                }
+            }
+        }
+    )
+}
--- presentation/src/main/java/com/hitec/presentation/main/MainNavHost.kt
+++ presentation/src/main/java/com/hitec/presentation/main/MainNavHost.kt
@@ -3,48 +3,38 @@ package com.hitec.presentation.main
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.hitec.presentation.R
 import com.hitec.presentation.main.asdevice.AsDeviceScreen
 import com.hitec.presentation.main.camera.CameraScreen
 import com.hitec.presentation.main.installdevice.InstallDeviceScreen
 import com.hitec.presentation.main.mypage.MyPageScreen
 
 @RequiresApi(Build.VERSION_CODES.R)
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainNavHost(sharedViewModel: MainViewModel) {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val scaffoldState = rememberBottomSheetScaffoldState()
+    val snackbarHostState = remember { SnackbarHostState() }
     val currentRoute = navBackStackEntry?.destination?.route
 
     Surface {
         Scaffold(
             topBar = {
-                TopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    ),
-                    title = {
-                        Text(text = stringResource(id = R.string.app_name))
-                    }
+                MainHeader(
+                    viewModel = sharedViewModel,
+                    navController = navController,
+                    currentRoute = currentRoute
                 )
             },
             content = { paddingValues ->
@@ -86,7 +76,8 @@ fun MainNavHost(sharedViewModel: MainViewModel) {
                         currentRoute = currentRoute
                     )
                 }
-            }
+            },
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
         )
     }
 }
