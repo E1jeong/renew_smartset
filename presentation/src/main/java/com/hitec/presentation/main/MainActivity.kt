a72d28d6cff5372c331034a71e4120fd079e3cb0
E1jeong <won9964@gmail.com>
2024-08-21 16:30:26 +0900

    add: MainActivity - onBackPressedDispatcher

--- presentation/src/main/java/com/hitec/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/hitec/presentation/main/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.hitec.presentation.main
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import com.hitec.presentation.login.LoginActivity
 import com.hitec.presentation.theme.RenewSmartSetTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +20,14 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
 
         setContent {
             RenewSmartSetTheme {
