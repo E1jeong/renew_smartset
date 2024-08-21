a5faa552f6af409ee0d5d6e8a02bf3671e24c78c
E1jeong <won9964@gmail.com>
2024-08-21 09:18:14 +0900

    chore: code format by module

--- data/src/main/java/com/hitec/data/di/HitecModule.kt
+++ data/src/main/java/com/hitec/data/di/HitecModule.kt
@@ -4,6 +4,8 @@ import com.hitec.data.usecase.DeleteInstallDbUseCaseImpl
 import com.hitec.data.usecase.FindLocalSiteNameUseCaseImpl
 import com.hitec.data.usecase.GetInstallDbUrlUseCaseImpl
 import com.hitec.data.usecase.GetInstallDbUseCaseImpl
+import com.hitec.data.usecase.GetInstallDeviceListFromImeiAndSubAreaUseCaseImpl
+import com.hitec.data.usecase.GetInstallDeviceListFromSubAreaUseCaseImpl
 import com.hitec.data.usecase.GetInstallDeviceUseCaseImpl
 import com.hitec.data.usecase.GetLocalSiteUseCaseImpl
 import com.hitec.data.usecase.GetSubAreaUseCaseImpl
@@ -14,6 +16,8 @@ import com.hitec.domain.usecase.GetInstallDbUseCase
 import com.hitec.domain.usecase.GetInstallDeviceUseCase
 import com.hitec.domain.usecase.GetLocalSiteUseCase
 import com.hitec.domain.usecase.GetSubAreaUseCase
+import com.hitec.domain.usecase.main.search.GetInstallDeviceListFromImeiAndSubAreaUseCase
+import com.hitec.domain.usecase.main.search.GetInstallDeviceListFromSubAreaUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -22,18 +26,37 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class HitecModule {
+
+    //======================================
+    //Retrofit module
+    //======================================
     @Binds
     abstract fun bindGetLocalSiteUseCase(uc: GetLocalSiteUseCaseImpl): GetLocalSiteUseCase
+
     @Binds
     abstract fun bindFindLocalSiteNameUseCase(uc: FindLocalSiteNameUseCaseImpl): FindLocalSiteNameUseCase
+
     @Binds
     abstract fun bindGetSubAreaUseCase(uc: GetSubAreaUseCaseImpl): GetSubAreaUseCase
+
     @Binds
     abstract fun bindGetInstallDbUrlUseCase(uc: GetInstallDbUrlUseCaseImpl): GetInstallDbUrlUseCase
+
     @Binds
     abstract fun bindGetInstallDbUseCase(uc: GetInstallDbUseCaseImpl): GetInstallDbUseCase
-    @Binds
-    abstract fun bindGetInstallDeviceUseCase(uc: GetInstallDeviceUseCaseImpl): GetInstallDeviceUseCase
+
     @Binds
     abstract fun bindDeleteInstallDbUseCase(uc: DeleteInstallDbUseCaseImpl): DeleteInstallDbUseCase
+
+    //======================================
+    //Room module
+    //======================================
+    @Binds
+    abstract fun bindGetInstallDeviceUseCase(uc: GetInstallDeviceUseCaseImpl): GetInstallDeviceUseCase
+
+    @Binds
+    abstract fun bindGetInstallDeviceListFromSubAreaUseCase(uc: GetInstallDeviceListFromSubAreaUseCaseImpl): GetInstallDeviceListFromSubAreaUseCase
+
+    @Binds
+    abstract fun bindGetInstallDeviceListFromImeiAndSubAreaUseCase(uc: GetInstallDeviceListFromImeiAndSubAreaUseCaseImpl): GetInstallDeviceListFromImeiAndSubAreaUseCase
 }
