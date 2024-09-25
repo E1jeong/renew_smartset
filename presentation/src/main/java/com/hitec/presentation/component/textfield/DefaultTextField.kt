4fddd1490a96104837910b53a3abd14a81989af3
E1jeong <won9964@gmail.com>
2024-09-25 10:06:41 +0900

    modify: DefaultTextField - apply placeholder

--- presentation/src/main/java/com/hitec/presentation/component/textfield/DefaultTextField.kt
+++ presentation/src/main/java/com/hitec/presentation/component/textfield/DefaultTextField.kt
@@ -1,10 +1,12 @@
 package com.hitec.presentation.component.textfield
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
@@ -13,12 +15,19 @@ import androidx.compose.ui.unit.dp
 fun DefaultTextField(
     modifier: Modifier,
     value: String,
+    placeholder: String = "",
     visualTransformation: VisualTransformation = VisualTransformation.None,
     onValueChange: (String) -> Unit
 ) {
     TextField(
         modifier = modifier,
         value = value,
+        placeholder = {
+            Text(
+                modifier = Modifier.alpha(0.5f),
+                text = placeholder
+            )
+        },
         onValueChange = onValueChange,
         shape = RoundedCornerShape(8.dp),
         colors = TextFieldDefaults.colors(
